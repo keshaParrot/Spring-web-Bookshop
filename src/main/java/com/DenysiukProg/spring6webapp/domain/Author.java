419d97201b77e8f30693b7bab234dd080c33a317
Іван <strelok1323@gmail.com>
2024-02-28 16:21:27 +0100

    master

--- src/main/java/com/DenysiukProg/spring6webapp/domain/Author.java
+++ src/main/java/com/DenysiukProg/spring6webapp/domain/Author.java
@@ -5,7 +5,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.boot.autoconfigure.web.WebProperties;
 
 import java.util.HashSet;
 import java.util.Objects;
