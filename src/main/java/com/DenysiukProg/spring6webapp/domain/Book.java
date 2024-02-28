f5bf4fe75962ae2904c9be4bfca29b62f664590a
Іван <strelok1323@gmail.com>
2024-02-28 01:33:19 +0100

    master

--- src/main/java/com/DenysiukProg/spring6webapp/domain/Book.java
+++ src/main/java/com/DenysiukProg/spring6webapp/domain/Book.java
@@ -17,6 +17,7 @@ public class Book {
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
+    //private Set<User> users = new HashSet<>();
     @ManyToOne
     private Publisher publisher;
 
---
+++ src/main/java/com/DenysiukProg/spring6webapp/domain/User.java
@@ -0,0 +1,70 @@
+package com.DenysiukProg.spring6webapp.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String FirstName;
+    private String LastName;
+    private String Email;
+    private String password;
+
+    //Set<Book> books = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /*public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }*/
+}
